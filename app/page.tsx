30d9eb86089751b3016cceb1807a9385807d0c0b
Klim Orlov <flameko-dan@yandex.ru>
2022-12-28 00:16:34 +0300

    fix: async JSX

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import IndexPage from 'pages/IndexPage/IndexPage';
+import { getProducts } from 'shared/api/api';
+import { IProduct } from 'shared/types/data';
 
-const Page = () => {
-  return <IndexPage />;
+const Page = async () => {
+  const products: IProduct[] = await getProducts();
+
+  return <IndexPage data={products} />;
 };
 
 export default Page;
--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -3,18 +3,19 @@ import Promo from 'entities/Promo/ui/Promo';
 import AddToBucket from 'features/addToBucket/ui/AddToBucket';
 import LikeProduct from 'features/likeProduct/ui/LikeProduct';
 import React from 'react';
-import { getProducts } from 'shared/api/api';
 import { IProduct } from 'shared/types/data';
 import styles from './IndexPage.module.scss';
 
-const IndexPage = async () => {
-  const products: IProduct[] = await getProducts();
+interface IIndexPage {
+  data: IProduct[] | [];
+}
 
+const IndexPage: React.FC<IIndexPage> = ({ data }) => {
   return (
     <main className={styles['index-page']}>
       <section className={styles['index-page__content']}>
         <Promo />
-        {products.map((item) => (
+        {data.map((item) => (
           <Product
             data={item}
             key={item.id}
